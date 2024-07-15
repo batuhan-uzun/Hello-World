7d04b76f1cb56cfb5d3275b4f69e6bddc2a4f963
batuh <batuh@DESKTOP-DK772UH>
2024-07-15 22:55:53 +0300

    Add project files.

---
+++ Hello World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Batuhan Uzun");
+            Console.WriteLine("Patika'nin uzman kadrosu ve tecrübeli egitmenleri sayesinde yazilim dünyasina hizli bir sekilde giris yapabilecegim için bu bootcampteyim!");
+        }
+    }
+}
